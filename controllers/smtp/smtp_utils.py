33198518897583021e2528e689f40a97b794a17e
Alfonso <81327113+foncheto@users.noreply.github.com>
2024-12-03 00:20:09 -0300

    Update smtp_utils.py

--- controllers/smtp/smtp_utils.py
+++ controllers/smtp/smtp_utils.py
@@ -43,6 +43,8 @@ def send_email_with_smtp(to_email, subject, client_id, client_name, client_messa
     #Función para enviar un correo electrónico utilizando SMTP con un diseño HTML.
     try:
         # Construir la ruta absoluta al archivo HTML
+        # print working directory
+        print(os.getcwd())
         html_path = "../../html/email_template.html"
 
         # Leer el archivo HTML de plantilla

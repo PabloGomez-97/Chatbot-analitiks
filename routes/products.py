fa4497b00f300fc1700ce8959261973b83bcadb2
Alfonso <81327113+foncheto@users.noreply.github.com>
2024-12-11 00:36:06 -0300

    Update products.py

--- routes/products.py
+++ routes/products.py
@@ -3,7 +3,6 @@ from utils.product_handlers import fetch_and_save_products_json
 
 products_bp = Blueprint('products', __name__)
 
-@products_bp.route('/update_products', methods=['GET'])
 def update_products():
     fetch_and_save_products_json()
     return "✅ Datos de productos actualizados exitosamente", 200
